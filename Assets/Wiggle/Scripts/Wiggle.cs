395e30fbf26379c7530343c37505c2e7a9b01913
JacDev <kuo741258@gmail.com>
2022-03-28 15:57:40 +0800

    更新 Wiggle

---
+++ Assets/Wiggle/Scripts/Wiggle.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wiggle : MonoBehaviour
+{
+    public float xScale, yScale;
+    public float speed;
+
+    Vector3 origin;
+
+    void Start()
+    {
+        origin = transform.position;    // 取得物件位置    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x, y;
+        x = Mathf.PerlinNoise(Time.time * speed, 0) - .5f;
+        y = Mathf.PerlinNoise(0, Time.time * speed) - .5f;
+
+        Vector3 value = new Vector3(x * xScale, y * yScale, 0);
+
+        transform.position = origin + value;
+    }
+}
