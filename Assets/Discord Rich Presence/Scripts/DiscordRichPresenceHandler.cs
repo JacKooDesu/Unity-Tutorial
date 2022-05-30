b21bbad0c5f1f7e63fcb0d2c387fdce7b7d43d3f
JacDev <kuo741258@gmail.com>
2022-05-31 00:40:11 +0800

    Discord Rich Presence added

---
+++ Assets/Discord Rich Presence/Scripts/DiscordRichPresenceHandler.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Discord;
+
+public class DiscordRichPresenceHandler : MonoBehaviour
+{
+    Discord.Discord discord;
+    Discord.Activity activity;
+    public string state = "狀態";
+    public string details = "遊戲內容";
+    public string iconName;
+
+    void Start()
+    {
+        discord = new Discord.Discord(980733539119153162, (System.UInt64)Discord.CreateFlags.Default);
+        var activityManager = discord.GetActivityManager();
+        activity = new Discord.Activity
+        {
+            State = state,
+            Details = details,
+            Assets = {
+                LargeImage = iconName
+            }
+        };
+
+        activityManager.UpdateActivity(activity, (res) =>
+        {
+            if (res == Discord.Result.Ok)
+                Debug.Log("Discord 狀態載入成功");
+            else
+                Debug.LogError($"Discord 狀態載入失敗 / {res}");
+        });
+    }
+
+    void Update()
+    {
+        discord.RunCallbacks();
+    }
+}
