f8266ec5316f14048daa3f40ea8ca5c73fcb8ed7
JacKooDesu <kuo741258@gmail.com>
2022-10-13 17:03:33 +0800

    Add test coroutine script

---
+++ Assets/Coroutine/Scripts/UnityCoroutine.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnityCoroutine : MonoBehaviour
+{
+    void Start()
+    {
+        StartCoroutine(Coroutine());
+    }
+
+    IEnumerator Coroutine()
+    {
+        yield return new WaitUntil(() => true);
+        yield return null;
+    }
+}
